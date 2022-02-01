1f0577cbee8f03f645f64808026846c3907f1664
gcatanese <gcatanese@yahoo.com>
2022-02-01 11:47:52 +0100

    Refactor to use session-based AdyenCheckout

--- checkout/src/app/components/checkout/checkout.component.ts
+++ checkout/src/app/components/checkout/checkout.component.ts
@@ -11,6 +11,9 @@ import { environment } from '../../../environments/environment';
 export class CheckoutComponent implements OnInit {
   @ViewChild('hook', { static: true }) hook: ElementRef;
   type: string = '';
+  sessionId: string = '';
+  redirectResult: string = '';
+
   clientKey: string = environment.adyenClientKey as string;
 
   constructor(
@@ -21,30 +24,8 @@ export class CheckoutComponent implements OnInit {
     this.hook = new ElementRef('');
   }
 
-  async initiatePayment(state: any, component: any) {
-    try {
-      this.apiService.initiatePayment(state.data).subscribe((res) => {
-        this.handleServerResponse(res, component);
-      });
-    } catch (error) {
-      console.error(error);
-      alert('Error occurred. Look at console for details');
-    }
-  }
-
-  async submitAdditionalDetails(state: any, component: any) {
-    try {
-      this.apiService.submitAdditionalDetails(state.data).subscribe((res) => {
-        this.handleServerResponse(res, component);
-      });
-    } catch (error) {
-      console.error(error);
-      alert('Error occurred. Look at console for details');
-    }
-  }
-
   handleServerResponse(res: any, component: any) {
-    if (res.action) {
+    if (res.action != null) {
       component.handleAction(res.action);
     } else {
       switch (res.resultCode) {
@@ -67,45 +48,84 @@ export class CheckoutComponent implements OnInit {
 
   ngOnInit(): void {
     this.type = this.route.snapshot.queryParamMap.get('type') || '';
+    this.sessionId = this.route.snapshot.queryParamMap.get('sessionId') || '';
+    this.redirectResult = this.route.snapshot.queryParamMap.get('redirectResult') || '';
 
-    this.apiService.getPaymentMethods().subscribe(
-      (res) => {
-        const configuration = {
-          paymentMethodsResponse: res,
-          clientKey: this.clientKey,
-          locale: 'en_US',
-          environment: 'test',
-          showPayButton: true,
-          paymentMethodsConfiguration: {
-            ideal: {
-              showImage: true,
-            },
-            card: {
-              hasHolderName: true,
-              holderNameRequired: true,
-              name: 'Credit or debit card',
-              amount: {
-                value: 1000,
-                currency: 'EUR',
-              },
-            },
-          },
-          onSubmit: (state: any, component: any) => {
-            if (state.isValid) {
-              this.initiatePayment(state, component);
-            }
-          },
-          onAdditionalDetails: (state: any, component: any) => {
-            this.submitAdditionalDetails(state, component);
-          },
-        };
-        const checkout = new AdyenCheckout(configuration);
+    if (!this.sessionId) {
 
-        checkout.create(this.type).mount(this.hook.nativeElement);
-      },
-      (error) => {
-        console.log('Error is: ', error);
-      }
-    );
+      this.apiService.sessions().subscribe(
+        (async res => {
+
+            // Create AdyenCheckout using Sessions response
+            const checkout = await this.createAdyenCheckout(res)
+
+            await checkout.create(this.type).mount(this.hook.nativeElement);
+        }),
+        (async error => {
+          console.log('Error is: ', error);
+        })
+      );
+    }
+    else {
+      // existing session: complete Checkout
+      this.finalizeCheckout();
+    }
+
   }
+
+  async createAdyenCheckout(session: any) {
+
+      const configuration = {
+          clientKey: this.clientKey,
+          locale: "en_US",
+          environment: "test",  // change to live for production
+          showPayButton: true,
+          session: session,
+          paymentMethodsConfiguration: {
+              ideal: {
+                  showImage: true
+              },
+              card: {
+                  hasHolderName: true,
+                  holderNameRequired: true,
+                  name: "Credit or debit card",
+                  amount: {
+                      value: 1000,
+                      currency: "EUR"
+                  }
+              },
+              paypal: {
+                  amount: {
+                      currency: "USD",
+                      value: 1000
+                  },
+                  environment: "test",
+                  countryCode: "US"   // Only needed for test. This will be automatically retrieved when you are in production.
+              }
+          },
+          onPaymentCompleted: (state: any, component: any) => {
+              this.handleServerResponse(state, component);
+          },
+          onError: (error: any, component: any) => {
+              console.error(error.name, error.message, error.stack, component);
+          }
+      };
+
+      return await AdyenCheckout(configuration);
+  }
+
+  // Some payment methods use redirects. This is where we finalize the operation
+  async finalizeCheckout() {
+    try {
+        // Create AdyenCheckout re-using existing Session
+        const checkout = await this.createAdyenCheckout({id: this.sessionId});
+
+        // Submit the extracted redirectResult (to trigger onPaymentCompleted() handler)
+        checkout.submitDetails({details: this.redirectResult});
+    } catch (error) {
+        console.error(error);
+        alert("Error occurred. Look at console for details");
+    }
+  }
+
 }
